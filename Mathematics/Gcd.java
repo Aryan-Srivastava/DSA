6a7440a00daf64dac15cce73990e6f02ac5be644
Aryan Srivastava <68139593+Aryan-Srivastava@users.noreply.github.com>
2021-07-27 17:02:59 +0530

    Revert "Mathematics folder"
    This reverts commit 96806705f6a451864dcf8c70cdba9dfcf68d66cd.

--- Mathematics/Gcd.java
+++
@@ -1,9 +0,0 @@
-public class Gcd {
-    public static void main(String[] args) {
-        int x = 19, y = 10;
-        System.out.println(gcd(x,y)); 
-    }
-    static int gcd(int x, int y) {
-        return x%y == 0 ? y : gcd(y,x%y);
-    }
-}
