4551a349d63067afd1b903ad8bc9542fe768721b
Aryan <aryanyash999@gmail.com>
2021-08-04 02:18:45 +0530

    Shift all zeros to the end

---
+++ Arrays/Questions/Shift_Zeros_at_End.java
@@ -0,0 +1,22 @@
+package Questions;
+
+public class Shift_Zeros_at_End {
+
+    static void pushZerosToEnd(int arr[], int n) {
+	    int count = 0;
+	    for(int i = 0; i < n; i++){
+	        if(arr[i] != 0){
+	            int temp = arr[count];
+	            arr[count] = arr[i];
+	            arr[i] = temp;
+	            count++;
+	        }
+	    }
+	}
+    public static void main(String[] args) {
+        int arr[] = {9, 0, 0, 8, 2};
+        pushZerosToEnd(arr, arr.length);
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
