bf90148237d3a4351836d940b94222994db3260c
Aryan <aryanyash999@gmail.com>
2021-09-16 01:07:50 +0530

    Square root using binary search

---
+++ Arrays/Questions/Sqrt_Using_Binary_Search.java
@@ -0,0 +1,23 @@
+package Questions;
+
+public class Sqrt_Using_Binary_Search {
+    public static void main(String[] args) {
+        int x = 9;
+        System.out.println(sqrt(x));
+    }
+
+    private static int sqrt(int x) {
+        if(x < 1) return 0;
+        int start = 0, end = x;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(mid == x/mid)
+                return mid;
+            else if(mid < x/mid)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return end;
+    }
+}
