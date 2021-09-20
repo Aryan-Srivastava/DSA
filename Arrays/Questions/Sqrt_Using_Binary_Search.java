529adda8d5891db731c6907efb897b28a3b1e5c1
Aryan <aryanyash999@gmail.com>
2021-09-21 02:08:41 +0530

    Precise sqrt using Binary search

--- Arrays/Questions/Sqrt_Using_Binary_Search.java
+++ Arrays/Questions/Sqrt_Using_Binary_Search.java
@@ -1,9 +1,12 @@
 package Questions;
 
+import java.text.DecimalFormat;
+
 public class Sqrt_Using_Binary_Search {
     public static void main(String[] args) {
-        int x = 9;
+        int x = 7;
         System.out.println(sqrt(x));
+        System.out.println(preciseSqrt(x));
     }
 
     private static int sqrt(int x) {
@@ -19,5 +22,21 @@ public class Sqrt_Using_Binary_Search {
                 end = mid - 1;
         }
         return end;
+    }
+
+    private static double preciseSqrt(int x){
+        double low = 0f, high = x;
+        while(high - low > 1e-6){
+            double mid = low + (high - low) / 2.0;
+            if(mid == x/mid)
+                return mid;
+            else if(mid < x/mid)
+                low = mid;
+            else 
+                high = mid;
+        }
+        DecimalFormat df = new DecimalFormat("#.###");
+        double ans = Double.parseDouble(df.format(low));
+        return ans;
     }
 }
