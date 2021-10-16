661ef86ff96e5eccbf2aae757966b3362c95e2ab
Aryan <aryanyash999@gmail.com>
2021-10-16 14:04:51 +0530

    capacity to ship packages within d days changed

--- Arrays/Questions/capacity_To_Ship_Packages_Within_dDays.java
+++ Arrays/Questions/capacity_To_Ship_Packages_Within_dDays.java
@@ -14,25 +14,27 @@ public class capacity_To_Ship_Packages_Within_dDays {
         int right = IntStream.of(weights).sum(); // sum
         int left = Arrays.stream(weights).max().getAsInt(); // max
         while (left < right) {
-            int mid = left + (right - left) / 2, need = 1, curr = 0;
-            need = canShip(weights, mid, need, curr);
-            if (need > days) {
-                left = mid + 1;
-            } else {
+            int mid = left + (right - left) / 2;
+            if (canShip(weights, mid, days)) {
                 right = mid;
+            } else {
+                left = mid + 1;
             }
         }
         return left;
     }
 
-    private static int canShip(int[] weights, int mid, int need, int curr) {
+    private static boolean canShip(int[] weights, int mid, int days) {
+        int need = 1, curr = 0;
         for (int w : weights) {
-            if (curr + w > mid) {
-                need++;
-                curr = 0;
-            }
             curr += w;
+            if (curr > mid) {
+                need++;
+                curr = w;
+            }
+            if(need > days)
+                return false;
         }
-        return need;
+        return true;
     }
 }
