88dcf0638558a2b88e6131c65ab80e17316a2f0b
Aryan <aryanyash999@gmail.com>
2021-11-06 13:43:07 +0530

    reverse without changing the space

---
+++ String/reverse.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class reverse {
+
+    public static void main(String[] args) {
+        String s = "I am Aryan Srivastava";
+        rev(s);
+    }
+
+    private static void rev(String s) {
+        int i = 0, j = s.length() - 1;
+        char[] arr = s.toCharArray();
+        while (i < j) {
+            if(arr[i] == ' ') {
+                i++;
+            }
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+         Arrays.toString(arr);
+         System.out.println(arr);
+    }
+}
